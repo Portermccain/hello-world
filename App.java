c8f0a7ab025ed47f4306629bf33968c44a476cd9
Porter <portermccain@gmail.com>
2015-10-18 16:05:36 -0700

    Create App.java
    prints "Hello World!"

---
+++ App.java
@@ -0,0 +1,9 @@
+/**
+ * This simple program prints "Hello World!" to the console
+ */
+ 
+public class App{
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
